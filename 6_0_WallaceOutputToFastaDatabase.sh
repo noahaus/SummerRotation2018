2764adb7563042c6015559f738f935f0b8d03be8
noahaus <40697188+noahaus@users.noreply.github.com>
2018-07-10 15:38:13 -0400

    Update 6_0_WallaceOutputToFastaDatabase.sh

--- 6_0_WallaceOutputToFastaDatabase.sh
+++ 6_0_WallaceOutputToFastaDatabase.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 clear
-
 #Prompts
-echo "Type the name of the file you would like to create a fasta database of"
-read wallace_output
 echo "Type the PREFIX name of the output file you desire"
 read final_output
 
 #This is the simple way of excluding the header. 
 #get the line count then subtract that value by 1.
 #Use that new value in the parameters of the tail command
-LINES=`cat $wallace_output | wc -l`
+LINES=`cat 4e_SRR094438.convergent_gene_fpkm.txt | wc -l`
 NO_HEADER=$(($LINES-1))
-tail -n $NO_HEADER $wallace_output > no_header.bed
+tail -n $NO_HEADER 4e_SRR094438.convergent_gene_fpkm.txt > no_header.bed
 
 #The file I am working with has many columns, but we only need a small subset of them
 #After extracting what is needed, combine the files for later processing
@@ -21,6 +18,29 @@ cut -f 1,7,8 no_header.bed > downstream_output.out
 cat downstream_output.out >> upstream_output.out
 cat upstream_output.out > temp_u_gibba_draft.bed
 
+LINES=`cat 4e_SRR094438.divergent_gene_fpkm.txt | wc -l`
+NO_HEADER=$(($LINES-1))
+tail -n $NO_HEADER 4e_SRR094438.divergent_gene_fpkm.txt > no_header.bed
+
+#The file I am working with has many columns, but we only need a small subset of them
+#After extracting what is needed, combine the files for later processing
+cut -f 1,3,4 no_header.bed > upstream_output.out
+cut -f 1,7,8 no_header.bed > downstream_output.out
+cat downstream_output.out >> upstream_output.out
+cat upstream_output.out >> temp_u_gibba_draft.bed
+
+LINES=`cat 4e_SRR094438.parallel_gene_fpkm.txt | wc -l`
+NO_HEADER=$(($LINES-1))
+tail -n $NO_HEADER 4e_SRR094438.parallel_gene_fpkm.txt > no_header.bed
+
+#The file I am working with has many columns, but we only need a small subset of them
+#After extracting what is needed, combine the files for later processing
+cut -f 1,3,4 no_header.bed > upstream_output.out
+cut -f 1,7,8 no_header.bed > downstream_output.out
+cat downstream_output.out >> upstream_output.out
+cat upstream_output.out >> temp_u_gibba_draft.bed
+
+
 #We need to add lcl| to the label whether it be Scf or ctg
 python 6_1_ChangeScfOrCTGTolcl.py temp_u_gibba_draft.bed u_gibba_draft.bed
 
@@ -28,10 +48,10 @@ python 6_1_ChangeScfOrCTGTolcl.py temp_u_gibba_draft.bed u_gibba_draft.bed
 #BLAST database, time to actually make the fasta file.
 #sort the bed file, then just create the fasta file
 bedtools sort -i u_gibba_draft.bed > "$final_output.bed"
-bedtools getfasta -fi Utricularia_gibba.4.1.fa -bed "$final_output.bed" -fo "$final_output.fa"
+uniq "$final_output.bed" > "unique_$final_output.bed"
+bedtools getfasta -fi Utricularia_gibba.4.1.fa -bed "unique_$final_output.bed" -fo "$final_output.fa"
 
 #Create the custom database that BLAST queries can be searched against
 makeblastdb -in "$final_output.fa" -input_type fasta -dbtype nucl -title "$final_output.database" -parse_seqids -out "$final_output.database"
 
-#example of how to call blastn
-#blastn -query ../4_sequences/scripts/query.fa -db /home/noahaus/noahaus_data/6_fact_checking/"$final_output.database"
+blastn -query ../4_sequences/scripts/query.fa -db /home/noahaus/noahaus_data/6_fact_checking/"$final_output.database"
